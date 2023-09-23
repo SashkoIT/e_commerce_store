bfc2c5171dc9bf02cd8a54f12a9d48206b27ab33
Alex Kolev <107081652+SashkoIT@users.noreply.github.com>
2023-09-23 14:26:51 +0300

    Update settings.py

--- e_commerce_store/settings.py
+++ e_commerce_store/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-==6&#udjcf45ws@!g$m)usbne#omz4osx6w1av(1jqmq1g-h2i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
